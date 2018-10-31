cf2300b10ab7ed99315fdc809f0ea62a6f1c58aa
Anton Prokopenko <anton.9090@bk.ru>
2018-10-31 20:42:19 +0300

    final2

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: [
-        require('autoprefixer'),
+        require('autoprefixer')({
+            browsers: ['ie 11', 'last 4 version']
+        }),
         require('postcss-flexbugs-fixes')
     ]
 }
