417a54291f07690a95ff2d4d7cc2bcce1b97539a
Anton Prokopenko <anton.9090@bk.ru>
2018-10-31 16:42:43 +0300

    fixes3

--- webpack.dev.js
+++ webpack.dev.js
@@ -42,10 +42,7 @@ module.exports = {
                 test: /\.(js|jsx)?$/, 
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['react', 'stage-0']
-                    }
+                    loader: 'babel-loader'
                 }
             },
             {   test: /\.(js|jsx)?$/, enforce: "pre", loader: "source-map-loader" },
